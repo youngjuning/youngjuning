890f1549266a602ef4b3e3b223783b472cc27b5f
Aaron Young <luozhu2021@gmail.com>
2025-04-08 10:41:49 +0800

    Update no-json-stringify-in-deps.js

--- packages/eslint-plugin-x-react-hooks/rules/no-json-stringify-in-deps.js
+++ packages/eslint-plugin-x-react-hooks/rules/no-json-stringify-in-deps.js
@@ -15,26 +15,25 @@ module.exports = {
     return {
       CallExpression(node) {
         if (
-          node.callee.type === 'Identifier' &&
+          node.callee?.type === 'Identifier' &&
           (
-            node.callee.name === 'useEffect' ||
-            node.callee.name === 'useLayoutEffect' ||
-            node.callee.name === 'useMemo' ||
-            node.callee.name === 'useCallback'
+            node.callee?.name === 'useEffect' ||
+            node.callee?.name === 'useLayoutEffect' ||
+            node.callee?.name === 'useMemo' ||
+            node.callee?.name === 'useCallback'
           ) &&
-          node.arguments.length > 1 &&
-          node.arguments[1].type === 'ArrayExpression'
+          node.arguments?.length > 1 &&
+          node.arguments[1]?.type === 'ArrayExpression'
         ) {
-          const dependencies = node.arguments[1].elements;
+          const dependencies = node.arguments[1]?.elements;
           dependencies.forEach(dependency => {
-            console.warn("检查依赖项:", dependency.type, dependency.callee.type)
             if (
               dependency &&
               dependency.type === 'CallExpression' &&
-              dependency.callee.type === 'MemberExpression' &&
-              dependency.callee.object.type === 'Identifier' &&
-              dependency.callee.object.name === 'JSON' &&
-              dependency.callee.property.name === 'stringify'
+              dependency.callee?.type === 'MemberExpression' &&
+              dependency.callee?.object?.type === 'Identifier' &&
+              dependency.callee?.object?.name === 'JSON' &&
+              dependency.callee?.property?.name === 'stringify'
             ) {
               context.report({
                 node: dependency,
